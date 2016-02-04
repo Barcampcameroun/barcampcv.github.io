f2f2b794275518dd2eef6b7b1cf9d640cc7092d9
Mike Cole <cole.mike@gmail.com>
2016-02-03 20:21:41 -0600

    Cleaned up js. Fixed scrolling offset.

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,59 +14,20 @@ function handleTopNavAnimation() {
 	var top=$(window).scrollTop();
 
 	if(top>10){
-		$('#site-nav').addClass('navbar-solid'); 
+		$('#site-nav').addClass('navbar-solid');
 	}
 	else{
-		$('#site-nav').removeClass('navbar-solid'); 
+		$('#site-nav').removeClass('navbar-solid');
 	}
 }
-
-/*
- * Registration Form
-*/
-
-$('#registration-form').submit(function(e){
-    e.preventDefault();
-    
-    var postForm = { //Fetch form data
-            'fname'     : $('#registration-form #fname').val(),
-            'lname'     : $('#registration-form #lname').val(),
-            'email'     : $('#registration-form #email').val(),
-            'cell'      : $('#registration-form #cell').val(),
-            'address'   : $('#registration-form #address').val(),
-            'zip'       : $('#registration-form #zip').val(),
-            'city'      : $('#registration-form #city').val(),
-            'program'   : $('#registration-form #program').val()
-    };
-
-    $.ajax({
-            type      : 'POST',
-            url       : './assets/php/contact.php',
-            data      : postForm,
-            dataType  : 'json',
-            success   : function(data) {
-                            if (data.success) {
-                                $('#registration-msg .alert').html("Registration Successful");
-                                $('#registration-msg .alert').removeClass("alert-danger");
-                                $('#registration-msg .alert').addClass("alert-success");
-                                $('#registration-msg').show();
-                            }
-                            else
-                            {
-                                $('#registration-msg .alert').html("Registration Failed");
-                                $('#registration-msg .alert').removeClass("alert-success");
-                                $('#registration-msg .alert').addClass("alert-danger");
-                                $('#registration-msg').show();
-                            }
-                        }
-        });
-});
 
 /*
  * SmoothScroll
 */
 
-smoothScroll.init();
+smoothScroll.init({
+	offset:50
+});
 
 $('#clock').countdown('2016/03/05 08:00:00', function(event) {
    $(this).html(event.strftime('%D days %H:%M:%S'));
